71b1a7d2c23f7f90d6e973c8c9cd26b264868d21
Teemu Helineva <teemu.helineva@gmail.com>
2024-02-04 23:23:39 +0200

    Return a PlainDate instead of Date in parseDate

--- exercise-1/tdd-mooc-small-steps/src/prices.mjs
+++ exercise-1/tdd-mooc-small-steps/src/prices.mjs
@@ -19,7 +19,7 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
-    const cost = calculateCost(age, type, convertDateToPlainDate(date), baseCost);
+    const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
@@ -35,7 +35,7 @@ function createApp(database) {
   
   function parseDate(dateString) {
     if (dateString) {
-      return new Date(dateString);
+      return Temporal.PlainDate.from(dateString);
     }
   }
 
