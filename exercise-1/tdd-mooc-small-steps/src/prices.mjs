b3318f987addd4a4b3868ee18b6aa5d525674c9f
Teemu Helineva <teemu.helineva@gmail.com>
2024-02-04 22:53:40 +0200

    Propagated the conversion one step up from isMonday to calculateReduction

--- exercise-1/tdd-mooc-small-steps/src/prices.mjs
+++ exercise-1/tdd-mooc-small-steps/src/prices.mjs
@@ -79,14 +79,14 @@ function createApp(database) {
 
   function calculateReduction(date) {
     let reduction = 0;
-    if (date && isMonday(date) && !isHoliday(date)) {
+    if (date && isMonday(convertDateToPlainDate(date)) && !isHoliday(date)) {
       reduction = 35;
     }
     return reduction;
   }
 
   function isMonday(date) {
-    return convertDateToPlainDate(date).dayOfWeek === 1;
+    return date.dayOfWeek === 1;
   }
 
   function isHoliday(date) {
