ae2989fcb171117f6a9e77c965dabb55aee36d18
Teemu Helineva <teemu.helineva@gmail.com>
2024-03-04 15:43:29 +0200

    Implemented rotateRight().

--- exercise-2/tdd-mooc-tetris/src/RotatingShape.mjs
+++ exercise-2/tdd-mooc-tetris/src/RotatingShape.mjs
@@ -13,6 +13,15 @@ export class RotatingShape {
         this.width = this.shape[0].length;
     }
 
+    rotateRight() {
+        let str = [];
+        for (let h = 0; h < this.height; h++) {
+            for (let w = 0; w < this.width; w++) { str.push(this.shape[this.width - 1 - w][h]); }
+            if (h < this.height - 1) str.push("\n");
+        }
+        return RotatingShape.fromString(str.join(""));
+    }
+
     toString() {
         return this.shape.join("\n") + "\n";
     }
