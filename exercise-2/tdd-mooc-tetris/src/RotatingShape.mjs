36cde2dc1b1b2c60e8bd2e2ee908ee0200bea0e8
Teemu Helineva <teemu.helineva@gmail.com>
2024-03-04 15:36:09 +0200

    Added height and width class fields to RotatingShape.

--- exercise-2/tdd-mooc-tetris/src/RotatingShape.mjs
+++ exercise-2/tdd-mooc-tetris/src/RotatingShape.mjs
@@ -2,9 +2,15 @@ export class RotatingShape {
     static fromString(str) {
         return new RotatingShape(str);
     }
+    
+    height;
+    width;
     shape;
+    
     constructor(str) {
         this.shape = str.split("\n").map(s => { return s.replace(/\s+/g, ""); })
+        this.height = this.shape.length;
+        this.width = this.shape[0].length;
     }
 
     toString() {
