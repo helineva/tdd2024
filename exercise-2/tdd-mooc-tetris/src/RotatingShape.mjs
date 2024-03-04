3d9419e3c4fb2f679835884bd6f1fd82b0ef9dcd
Teemu Helineva <teemu.helineva@gmail.com>
2024-03-04 15:21:19 +0200

    Made the code in RotatingShape more readable.

--- exercise-2/tdd-mooc-tetris/src/RotatingShape.mjs
+++ exercise-2/tdd-mooc-tetris/src/RotatingShape.mjs
@@ -1,6 +1,13 @@
 export class RotatingShape {
-    static fromString(str) { return new RotatingShape(str); }
+    static fromString(str) {
+        return new RotatingShape(str);
+    }
     shape;
-    constructor(str) { this.shape = str.split("\n").map(s => { return s.replace(/\s+/g, ""); }) }
-    toString() { return this.shape.join("\n") + "\n"; }
+    constructor(str) {
+        this.shape = str.split("\n").map(s => { return s.replace(/\s+/g, ""); })
+    }
+
+    toString() {
+        return this.shape.join("\n") + "\n";
+    }
 }
