b3be5f51583d9b548607600518b6fb4b6871fc71
Teemu Helineva <teemu.helineva@gmail.com>
2024-03-04 14:02:38 +0200

    Added a new class field to describe whether a block is falling on the board at the moment.

--- exercise-2/tdd-mooc-tetris/src/Board.mjs
+++ exercise-2/tdd-mooc-tetris/src/Board.mjs
@@ -2,28 +2,37 @@ export class Board {
   width;
   height;
   board;
+  falling;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.board = [];
+    this.falling = false;
     for (let i = 0; i < this.height; i++) {
       this.board.push(Array(this.width).fill("."));
     }
   }
 
   drop(block) {
+    if (this.falling) {
+      throw new Error("already falling")
+    }
     this.board[0][1] = "X";
+    this.falling = true;
   }
 
   tick() {
+    let changed = false
     for (let i = this.height - 1; i > 0; i--) {
       for (let j = 0; j < this.width; j++) {
         if (this.board[i-1][j] === "X") {
           this.board[i-1][j] = ".";
           this.board[i][j] = "X";
+          changed = true
         }
       }
+    this.falling = changed;
   }}
 
   toString() {
