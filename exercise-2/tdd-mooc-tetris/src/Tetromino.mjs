6195336a2754d65a8eae5ea31f1dcfc096e699e1
Teemu Helineva <teemu.helineva@gmail.com>
2024-03-05 15:07:53 +0200

    Added class fields to describe (valid) orientations.

--- exercise-2/tdd-mooc-tetris/src/Tetromino.mjs
+++ exercise-2/tdd-mooc-tetris/src/Tetromino.mjs
@@ -6,13 +6,17 @@ export class Tetromino {
     }
 
     static get I_SHAPE() {
-        return new Tetromino(RotatingShape.fromString(".....\n.....\nIIII.\n.....\n....."));
+        return new Tetromino(RotatingShape.fromString(".....\n.....\nIIII.\n.....\n....."), 0, [true, true, false, false]);
     }
 
     shape;
+    orientation;
+    valid_orientations;
 
-    constructor(shape) {
+    constructor(shape, orientation=0, valid_orientations=[true, true, true, true]) {
         this.shape = shape;
+        this.orientation = orientation;
+        this.valid_orientations = valid_orientations;
     }
 
     rotateLeft() {
