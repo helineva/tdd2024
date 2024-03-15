e227af4b336809960ff34d1052198333d5c9ba3d
Teemu Helineva <teemu.helineva@gmail.com>
2024-03-15 23:45:23 +0200

    Further modifications towards handling tetrominos.

--- exercise-2/tdd-mooc-tetris/src/Board.mjs
+++ exercise-2/tdd-mooc-tetris/src/Board.mjs
@@ -19,6 +19,7 @@ export class Board {
 
   placeBlock(y, x) {
     let success = true;
+
     for (let j = 0; j < this.fallingBlock.shape.sideLength; j++) {
       for (let i = 0; i < this.fallingBlock.shape.sideLength; i++) {
         if (this.fallingBlock.shape.layout[j][i] !== ".") {
@@ -26,7 +27,9 @@ export class Board {
         }
       }
     }
+
     if (!success) return false;
+    
     for (let j = 0; j < this.fallingBlock.shape.sideLength; j++) {
       for (let i = 0; i < this.fallingBlock.shape.sideLength; i++) {
         if (this.fallingBlock.shape.layout[j][i] !== ".") {
@@ -36,11 +39,10 @@ export class Board {
         }
       }
     }
-    if (success) {
-      this.fallingBlockX = x;
-      this.fallingBlockY = y;
-    }
-    return success;
+    
+    this.fallingBlockX = x;
+    this.fallingBlockY = y;  
+    return true;
   }
 
   drop(block) {
