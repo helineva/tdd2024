29807662a82edf26337eeee098e76e83716fa77e
Teemu Helineva <teemu.helineva@gmail.com>
2024-03-18 18:35:13 +0200

    Tested for and implemented moving falling tetrominos.

--- exercise-2/tdd-mooc-tetris/test/MovingTetrominoes.test.mjs
+++ exercise-2/tdd-mooc-tetris/test/MovingTetrominoes.test.mjs
@@ -20,7 +20,7 @@ describe("Falling tetrominoes", () => {
          ..........
          ..........
          ..........`
-      );
+    );
   });
 
   test("can be moved right", () => {
@@ -33,7 +33,7 @@ describe("Falling tetrominoes", () => {
          ..........
          ..........
          ..........`
-      );
+    );
   });
 
   test("can be moved down", () => {
@@ -46,6 +46,119 @@ describe("Falling tetrominoes", () => {
          ..........
          ..........
          ..........`
-      );
+    );
+  });
+
+  test("can not be moved left beyond the board", () => {
+    board.drop(Tetromino.T_SHAPE);
+    for (let i = 0; i < 4; i++) {
+        board.moveLeft();
+    }
+
+    expect(board.toString()).to.equalShape(
+        `.T........
+         TTT.......
+         ..........
+         ..........
+         ..........
+         ..........`
+    );
+  });
+
+  test("can not be moved right beyond the board", () => {
+    board.drop(Tetromino.T_SHAPE);
+    for (let i = 0; i < 5; i++) {
+        board.moveRight();
+    }
+
+    expect(board.toString()).to.equalShape(
+        `........T.
+         .......TTT
+         ..........
+         ..........
+         ..........
+         ..........`
+    );
+  });
+
+  test("can not be moved down beyond the board", () => {
+    board.drop(Tetromino.T_SHAPE);
+    for (let i = 0; i < 5; i++) {
+        board.moveDown();
+    }
+
+    expect(board.toString()).to.equalShape(
+        `..........
+         ..........
+         ..........
+         ..........
+         ....T.....
+         ...TTT....`
+    );
+  });
+
+  test("can not be moved down through other blocks", () => {
+    board.drop(Tetromino.T_SHAPE);
+    for (let i = 0; i < 5; i++) board.tick();
+
+    board.drop(Tetromino.T_SHAPE);
+    for (let i = 0; i < 3; i++) board.moveDown();
+    
+    expect(board.toString()).to.equalShape(
+        `..........
+         ..........
+         ....T.....
+         ...TTT....
+         ....T.....
+         ...TTT....`
+    );
+  });
+
+  test("can not be moved left through other blocks", () => {
+    board.drop(Tetromino.T_SHAPE);
+    for (let i = 0; i < 3; i++) board.moveLeft();
+    for (let i = 0; i < 5; i++) board.tick();
+
+    board.drop(Tetromino.T_SHAPE);
+    for (let i = 0; i < 3; i++) board.moveLeft();
+    for (let i = 0; i < 5; i++) board.tick();
+    
+    board.drop(Tetromino.T_SHAPE);
+    board.tick()
+    board.moveLeft();
+    board.moveLeft();
+    
+    expect(board.toString()).to.equalShape(
+        `..........
+         ...T......
+         .TTTT.....
+         TTT.......
+         .T........
+         TTT.......`
+    );
+  });
+
+  test("can not be moved right through other blocks", () => {
+    board.drop(Tetromino.T_SHAPE);
+    for (let i = 0; i < 3; i++) board.moveRight();
+    for (let i = 0; i < 5; i++) board.tick();
+
+    board.drop(Tetromino.T_SHAPE);
+    for (let i = 0; i < 3; i++) board.moveRight();
+    for (let i = 0; i < 5; i++) board.tick();
+    
+    board.drop(Tetromino.T_SHAPE);
+    board.tick()
+    board.moveRight();
+    board.moveRight();
+    
+    expect(board.toString()).to.equalShape(
+        `..........
+         .....T....
+         ....TTTT..
+         ......TTT.
+         .......T..
+         ......TTT.`
+    );
   });
 });
