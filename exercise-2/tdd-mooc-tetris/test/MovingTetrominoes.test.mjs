f295b4a2497429b19d95b31c7dfd28423f1bbdd3
Teemu Helineva <teemu.helineva@gmail.com>
2024-03-18 17:49:02 +0200

    Writing the first test for moving tetrominos.

--- exercise-2/tdd-mooc-tetris/test/MovingTetrominoes.test.mjs
+++ exercise-2/tdd-mooc-tetris/test/MovingTetrominoes.test.mjs
@@ -4,13 +4,13 @@ import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
 import { Tetromino } from "../src/Tetromino.mjs";
 
-describe("Moving tetrominoes", () => {
+describe("Falling tetrominoes", () => {
   let board;
   beforeEach(() => {
     board = new Board(10, 6);
   });
 
-  test("start from the top middle", () => {
+  test("can be moved left", () => {
     board.drop(Tetromino.T_SHAPE);
   });
 });
