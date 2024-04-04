c9f638179dcf27bbefdeeeb6bcabf7bc7e811d20
Teemu Helineva <teemu.helineva@gmail.com>
2024-04-04 22:48:08 +0300

    Implementing ShuffleBag.

--- exercise-2/tdd-mooc-tetris/src/ShuffleBag.mjs
+++ exercise-2/tdd-mooc-tetris/src/ShuffleBag.mjs
@@ -1,8 +1,17 @@
 export class ShuffleBag {
     bag;
+    currIndex;
     constructor() {
         this.bag = [];
+        this.currIndex = 0;
     }
     fill(block) { this.bag.push(block); }
-    get() { return this.bag.pop(); }
+    get() {
+        if (this.currIndex === this.bag.length-1) {
+            this.currIndex = 0;
+            return this.bag[this.bag.length-1];
+        }
+        this.currIndex++;
+        return this.bag[this.currIndex-1];
+    }
 }
--- exercise-2/tdd-mooc-tetris/test/ShuffleBag.test.mjs
+++ exercise-2/tdd-mooc-tetris/test/ShuffleBag.test.mjs
@@ -15,4 +15,26 @@ describe("ShuffleBag", () => {
         expect(bag.get()).to.equal(block);
     });
 
+    test("returns the block it was filled with many times", () => {
+        let block = ArikaTetromino.T_SHAPE;
+        bag.fill(block);
+        expect(bag.get()).to.equal(block);
+        expect(bag.get()).to.equal(block);
+    });
+
+    test("returns the blocks it was filled with", () => {
+        let blocks = [];
+        for (let i = 0; i < 10; i++) {
+            let block = ArikaTetromino.T_SHAPE;
+            blocks.push(block);
+            bag.fill(block);
+        }
+        for (let i = 0; i < 10; i++) {
+            let block = bag.get();
+            let index = blocks.findIndex((x) => x === block);
+            expect(index).to.not.equal(-1);
+            blocks.splice(index, 1);
+        }
+    });
+
 });
