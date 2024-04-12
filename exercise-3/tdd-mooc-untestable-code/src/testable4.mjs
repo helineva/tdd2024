31719a874ac8244dc7e8a037c1cf3d4bb7e5bf0b
Teemu Helineva <teemu.helineva@gmail.com>
2024-04-12 15:18:16 +0300

    Made example 4 testable.

---
+++ exercise-3/tdd-mooc-untestable-code/src/testable4.mjs
@@ -0,0 +1,46 @@
+import argon2 from "@node-rs/argon2";
+
+export class PostgresUserDao {
+  db;
+
+  constructor(db) { this.db = db; }
+
+  #rowToUser(row) {
+    return { userId: row.user_id, passwordHash: row.password_hash };
+  }
+
+  async getById(userId) {
+    const { rows } = await this.db.query(
+      `select user_id, password_hash
+       from users
+       where user_id = $1`,
+      [userId]
+    );
+    return rows.map(this.#rowToUser)[0] || null;
+  }
+
+  async save(user) {
+    await this.db.query(
+      `insert into users (user_id, password_hash)
+       values ($1, $2)
+       on conflict (user_id) do update
+           set password_hash = excluded.password_hash`,
+      [user.userId, user.passwordHash]
+    );
+  }
+}
+
+export class PasswordService {
+  users;
+
+  constructor(users) { this.users = users; }
+
+  async changePassword(userId, oldPassword, newPassword) {
+    const user = await this.users.getById(userId);
+    if (!argon2.verifySync(user.passwordHash, oldPassword)) {
+      throw new Error("wrong old password");
+    }
+    user.passwordHash = argon2.hashSync(newPassword);
+    await this.users.save(user);
+  }
+}
