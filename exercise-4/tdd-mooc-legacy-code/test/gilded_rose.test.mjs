a00b1107eea052b6acdd4e0e1aed04964a58c3da
Teemu Helineva <teemu.helineva@gmail.com>
2024-04-14 23:18:14 +0300

    Increased line coverage to 60.31%

--- exercise-4/tdd-mooc-legacy-code/test/gilded_rose.test.mjs
+++ exercise-4/tdd-mooc-legacy-code/test/gilded_rose.test.mjs
@@ -3,9 +3,9 @@ import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
 describe("Gilded Rose", () => {
-  test("foo", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+  test("items other than Brie, Backstage passes and Sulfuras with positive quality and nonnegative sellIn decrease in quality by one", () => {
+    const gildedRose = new Shop([new Item("Jar of vatniksoup", 0, 1)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
+    expect(items[0].quality).to.equal(0);
   });
 });
