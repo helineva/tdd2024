3bb030fba95e03d96b8f337ef30fe9df0d5bcc35
Teemu Helineva <teemu.helineva@gmail.com>
2024-04-14 23:32:04 +0300

    Increased line coverage to 69.84%

--- exercise-4/tdd-mooc-legacy-code/test/gilded_rose.test.mjs
+++ exercise-4/tdd-mooc-legacy-code/test/gilded_rose.test.mjs
@@ -8,4 +8,18 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items[0].quality).to.equal(0);
   });
+
+  test("Aged Brie with quality < 50 and sellIn > 0 increases in quality by one", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 1), new Item("Aged Brie", 3, 5)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(2);
+    expect(items[1].quality).to.equal(6);
+  });
+
+  test("Aged Brie with quality < 50 and sellIn <= 0 increases in quality by two", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 1), new Item("Aged Brie", -3, 5)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(3);
+    expect(items[1].quality).to.equal(7);
+  });
 });
