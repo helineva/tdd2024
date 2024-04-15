70163e0a6c0e5160705667f21419086ad3167395
Teemu Helineva <teemu.helineva@gmail.com>
2024-04-15 19:04:16 +0300

    Refactoring code.

--- exercise-4/tdd-mooc-legacy-code/src/gilded_rose.mjs
+++ exercise-4/tdd-mooc-legacy-code/src/gilded_rose.mjs
@@ -25,8 +25,28 @@ export class Shop {
           }
           item.quality += (item.sellIn < 0) ? 2 : 1;
           break;
+        case "Backstage passes to a TAFKAL80ETC concert":
+          if (item.sellIn < 0) item.quality = 0;
+          else {
+            if (item.quality >= 50) break;
+            if (item.quality == 49) {
+              item.quality = 50;
+              break;
+            }
+            if (item.quality == 48) {
+              item.quality = item.sellIn >= 10 ? 49 : 50;
+              break
+            }
+            if (item.sellIn >= 0 && item.sellIn <= 4) item.quality += 3;
+            if (item.sellIn >= 5 && item.sellIn <= 9) item.quality += 2;
+            if (item.sellIn >= 10) item.quality += 1;
+            break;
+          }
       }
       if (item.name == "Aged Brie") {
+        continue;
+      }
+      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
         continue;
       }
       if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
