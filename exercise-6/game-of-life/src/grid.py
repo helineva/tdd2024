e93b9279f3a8033a2309cf56e21c768bc81d54b2
Teemu Helineva <teemu.helineva@gmail.com>
2024-04-28 15:08:19 +0300

    Implementing grid.

--- exercise-6/game-of-life/src/grid.py
+++ exercise-6/game-of-life/src/grid.py
@@ -1,13 +1,35 @@
 class Grid:
-    def __init__(self, s, width=1, height=1):
+    def __init__(self, seed, width=1, height=1):
         self.width = width
         self.height = height
-        self.grid = [c not in (False, ".") for c in s]
+        self.curr_grid = [c not in (False, ".") for c in seed]
+        self.next_grid = [False]*(width*height)
+ 
+    def __generate_neighbours(self, row, col):
+        dx = [1, 1, 1, 0, 0, -1, -1, -1]
+        dy = [1, 0, -1, 1, -1, 1, 0, -1]
+        for i in range(8):
+            if 0 <= row+dy[i] < self.height and 0 <= col+dx[i] < self.width:
+                yield (row+dy[i], col+dx[i])
 
+    def tick(self):
+        for row in range(self.height):
+            for col in range(self.width):
+                live_cell_count = 0
+                for nb_row, nb_col in self.__generate_neighbours(row, col):
+                    if self.curr_grid[nb_row*self.width + nb_col]:
+                        live_cell_count += 1
+                if live_cell_count == 0:
+                    self.next_grid[row*self.width + col] = False
+                else:
+                    self.next_grid[row*self.width + col] = self.curr_grid[row*self.width + col]
+        self.curr_grid, self.next_grid = self.next_grid, self.curr_grid
+        
+    
     def __str__(self):
         s = []
         for row in range(self.height):
             for col in range(self.width):
-                s.append("." if not self.grid[row*self.width+col] else "X")
+                s.append("." if not self.curr_grid[row*self.width+col] else "X")
             s.append("\n")
         return "".join(s)
--- exercise-6/game-of-life/tests/test_grid.py
+++ exercise-6/game-of-life/tests/test_grid.py
@@ -7,20 +7,64 @@ def test_create_grid():
 
 def test_create_grid_described_by_string():
     """can create a grid described by a string"""
-    s = ("...." +
+    seed = ("...." +
          ".X.." +
          "....")
-    grid = Grid(s, width=4, height=3)
+    grid = Grid(seed, width=4, height=3)
     assert str(grid) == ("....\n" +
                          ".X..\n" +
                          "....\n")
     
 def test_create_grid_described_by_boolean_array():
     """can create a grid described by a boolean array"""
-    s = ([False, False, False] +
+    seed = ([False, False, False] +
          [False, True, False] +
          [False, False, True])
-    grid = Grid(s, width=3, height=3)
+    grid = Grid(seed, width=3, height=3)
     assert str(grid) == ("...\n" +
                          ".X.\n" +
                          "..X\n")
+
+def test_grid_without_live_cells_does_not_change():
+    """a grid with no live cells remains that way"""
+    seed = ("...." +
+            "...." +
+            "....")
+    grid = Grid(seed, width=4, height=3)
+    grid.tick()
+    assert str(grid) == ("....\n" +
+                         "....\n" +
+                         "....\n")
+
+def test_live_cell_without_live_neighbours_dies_corner():
+    """a live cell without live neighbours dies, at the corner"""
+    seed = ("X..." +
+            "...." +
+            "....")
+    grid = Grid(seed, width=4, height=3)
+    grid.tick()
+    assert str(grid) == ("....\n" +
+                         "....\n" +
+                         "....\n")
+    
+def test_live_cell_without_live_neighbours_dies_edge():
+    """a live cell without live neighbours dies, on the edge"""
+    seed = (".X.." +
+            "...." +
+            "....")
+    grid = Grid(seed, width=4, height=3)
+    grid.tick()
+    assert str(grid) == ("....\n" +
+                         "....\n" +
+                         "....\n")
+    
+def test_live_cell_without_live_neighbours_dies_inside():
+    """a live cell without live neighbours dies, on the inside"""
+    seed = ("...." +
+            ".X.." +
+            "....")
+    grid = Grid(seed, width=4, height=3)
+    grid.tick()
+    assert str(grid) == ("....\n" +
+                         "....\n" +
+                         "....\n")
