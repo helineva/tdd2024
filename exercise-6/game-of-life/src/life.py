006d3a2279d27db3b075b0e7f44f219194d6185b
Teemu Helineva <teemu.helineva@gmail.com>
2024-05-06 14:55:25 +0300

    Implemented main function.

--- exercise-6/game-of-life/src/life.py
+++ exercise-6/game-of-life/src/life.py
@@ -1,5 +1,7 @@
 import grid
+from pathlib import Path
 import rle
+import sys
 
 def play(input_file, gen, output_file):
     if not input_file.is_file():
@@ -7,7 +9,7 @@ def play(input_file, gen, output_file):
     if output_file.is_file():
         raise Exception("output file already exists")
 
-    with open(input_file, "r") as f:
+    with open(input_file, "r", encoding="utf-8") as f:
         input_rle = f.read()
     
     seed, width, height = rle.decode(input_rle)
@@ -21,5 +23,18 @@ def play(input_file, gen, output_file):
     height = g.get_height()
     output_rle = rle.encode(pattern, width, height)
 
-    with open(output_file, "w") as f:
+    with open(output_file, "w", encoding="utf-8") as f:
         f.write(output_rle)
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) < 4:
+        print("Usage: life.py <input file in RLE format> <number of generations to simulate> <output file>")
+        sys.exit()
+    
+    input_file = Path(args[1])
+    gen = int(args[2])
+    output_file = Path(args[3])
+
+    play(input_file, gen, output_file)
+    
